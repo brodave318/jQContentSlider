bb73a81ec4e79943d6dfe33033c4adcc385355bf
David <brodave318@gmail.com>
2017-06-03 17:25:36 -0700

    Added jQuery markup

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  // Set Options
+  var speed = 500;              // Fade speed
+  var autoswitch = true;        // Auto slider options
+  var autoswitch_speed = 4000   // Auto slider speed
+
+
+  // Add intial active class
+  $('.slide').first().addClass('active');
+
+  // Hide all slides
+  $('.slide').hide();
+
+  // Show first slide
+  $('.active').show();
+
+  // Next handler
+  $('#next').on('click', nextSlide);
+
+  // Previous handler
+  $('#prev').on('click', prevSlide);
+
+  // Auto slider handler
+  if(autoswitch == true) {
+    setInterval(nextSlide, autoswitch_speed);
+  }
+
+  //Switch to the next slide
+  function nextSlide() {
+    $('.active').removeClass('active').addClass('oldActive');
+    if($('.oldActive').is(':last-child')) {
+      $('.slide').first().addClass('active');
+    } else {
+      $('.oldActive').next().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $('.slide').fadeOut(speed);
+    $('.active').fadeIn(speed);
+  }
+
+  //Switch to previous slide
+  function prevSlide() {
+    $('.active').removeClass('active').addClass('oldActive');
+    if($('.oldActive').is(':first-child')) {
+      $('.slide').last().addClass('active');
+    } else {
+      $('.oldActive').prev().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $('.slide').fadeOut(speed);
+    $('.active').fadeIn(speed);
+  }
+});
